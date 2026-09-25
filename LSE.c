e37c41f9f6147cb89a9a602d1b863c5fd3765b18
muriloataide <lilosat6@gmail.com>
2026-09-25 00:27:47 -0300

    Update LSE.c

--- LSE.c
+++ LSE.c
@@ -73,11 +73,11 @@ int lse_insere_pos(LSE *l,int pos, LSE_Tipo elem){
     if(!l){
         return -1;
     }
-    if(lse_cheia(l) == 1){
+    if(lse_cheia(l)){
         return 0;
     }
-    if(pos < 0 || pos > MAX){
-        return -1;
+    if(pos < 0 || pos > l->qtd){
+        return 0;
     }
     for(int i = l->qtd; i > pos; i--){
         l->dados[i] = l->dados[i-1]; 
@@ -93,13 +93,15 @@ int lse_remove_pos(LSE *l, int pos, LSE_Tipo *removido){
     if(!l){
         return -1;
     }
-    if(lse_cheia(l) == 1){
-        return 0;
-    }
-    if(pos < 0 || pos > MAX){
+    
+    if(pos < 0 || pos >= l->qtd){
         return -1;
     }
-    *removido = l->dados[pos];
+
+    if(removido != NULL){
+        *removido = l->dados[pos];
+    }
+
     for(int i = pos; i < l->qtd - 1; i++){
         l->dados[i] = l->dados[i+1];
     }
@@ -112,10 +114,14 @@ int lse_consulta_pos(const LSE *l, int pos, LSE_Tipo *elem){
     if(!l || !elem){
         return -1;
     }
-    if(pos < 0 || pos > MAX){
+
+    if(pos < 0 || pos >= l->qtd){
         return -1;
     }
+
     *elem = l->dados[pos];
+
+    return 1;
 }
 
 int lse_busca(const LSE *l,LSE_Tipo elem, int *pos){
@@ -147,11 +153,16 @@ int lse_imprime(const LSE *l){
     if(!l){
         return -1;
     }
-
-    for(int i = 0; i < l->qtd; i++){
-        printf("%d ",l->dados[i]);
+    printf("["); //alteração realizada para formato esperado em exercício de sala de aula
+    for(int i = 0; i < l->qtd-1; i++){
+        printf("%d, ",l->dados[i]);
     }
-    printf("\n");
+    if(lista_vazia(l) != 1){                    //alteracoes feitas para
+        printf("%d]\n",l->dados[l->qtd-1]);     //formato de exercício
+    }       
+    else{
+        printf("]\n");
+    }                               
     return 1;
 }
 
